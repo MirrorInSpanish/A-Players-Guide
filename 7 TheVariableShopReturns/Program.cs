92110391cf98395971f05e859e558ad815ad6cf2
Andrew Espejo <166669925+MirrorInSpanish@users.noreply.github.com>
2025-01-14 18:52:27 -0500

    Add files via upload

---
+++ 7 TheVariableShopReturns/Program.cs
@@ -0,0 +1,56 @@
+﻿
+//original declarations and assignments of variables
+sbyte a = -1;
+byte b = 1;
+
+int c = -2;
+uint d = 2;
+
+short e = -3;
+ushort f = 3;
+
+long g = -4L;
+ulong h = 4UL;
+
+float i = 5.0F;
+double j = 5.00000005;
+decimal k = 5.4039M;
+
+char l = 'A';
+string m = "Andrew";
+
+bool n = true;
+bool o = false;
+
+//reassignment of variables
+a = -2;
+b = 2;
+c = -3;
+d = 3;
+e = -4;
+f = 4;
+g = -5;
+h = 5;
+i = 5f;
+j = 5;
+k = 5m;
+l = 'O';
+m = "Oliver";
+n = false;
+o = true;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+Console.WriteLine(l);
+Console.WriteLine(m);
+Console.WriteLine(n);
+Console.WriteLine(o);
