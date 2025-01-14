02865242b26610d8d5f05339fc0cf8fc73de9b3a
Andrew Espejo <166669925+MirrorInSpanish@users.noreply.github.com>
2025-01-14 18:59:44 -0500

    Add files via upload

---
+++ 06 TheVariableShop/Program.cs
@@ -0,0 +1,37 @@
+﻿sbyte a     = -1;
+byte b      = 1;
+
+int c       = -2;
+uint d      = 2;
+
+short e     = -3;
+ushort f    = 3;
+
+long g      = -4L;
+ulong h     = 4UL;
+
+float i     = 5.0F;
+double j    = 5.00000005;
+decimal k   = 5.4039M;
+
+char l      = 'A';
+string m    = "Andrew";
+
+bool n      = true;
+bool o      = false;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+Console.WriteLine(l);
+Console.WriteLine(m);
+Console.WriteLine(n);
+Console.WriteLine(o);
