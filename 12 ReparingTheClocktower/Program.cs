20fbd898dbb24eefa3d0330b34bd6e9156a1e7eb
Andrew Espejo <166669925+MirrorInSpanish@users.noreply.github.com>
2025-01-14 18:54:55 -0500

    Add files via upload

---
+++ 12 ReparingTheClocktower/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter a number from 1 - 12");
+
+int ticker = Console.Read();
+
+if (ticker % 2 == 0)
+{
+    Console.WriteLine("Tick");
+}
+else
+{
+    Console.WriteLine("Tock");
+}
