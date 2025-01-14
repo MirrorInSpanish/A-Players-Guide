7581596c0acfe0e54c885631ee4cebeca7131c88
Andrew Espejo <166669925+MirrorInSpanish@users.noreply.github.com>
2025-01-14 18:59:27 -0500

    Add files via upload

---
+++ 05 TheThingNamer3000/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+
+//asks for user input for name of "the thing" to assign string variable a with that input value
+string a = Console.ReadLine();
+
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+
+//asks for user input for description of "the thing" to assign string variable b with that input value
+string b = Console.ReadLine();
+
+//assigns string variable c with the value "of Doom"
+string c = "of Doom";
+
+//assigns string variable d with the value "3000"
+string d = "3000";
+
+Console.WriteLine("The " + b + " " + a + " " + c + " " + d + "!");
