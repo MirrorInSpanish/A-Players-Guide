9965c8c9d269fb3131af2d06b975865b246a5814
Andrew Espejo <166669925+MirrorInSpanish@users.noreply.github.com>
2025-01-14 18:58:45 -0500

    Add files via upload

---
+++ 03 TheMakingsOfAProgrammer/Program.cs
@@ -0,0 +1,10 @@
+﻿int x = 5;
+int y = 2;
+int z = x * y;
+
+Console.WriteLine("I can do simple operations if you'd like?\n");
+Console.WriteLine("What is the product of z = x * y, when x holds the value of 5 and y holds the value of 2?\n");
+Console.WriteLine(z);
+Console.WriteLine("\n");
+Console.WriteLine("Thus, x * y = 10\n");
+Console.WriteLine("Are you not entertained?!");
